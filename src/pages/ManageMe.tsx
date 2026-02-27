df6f128d7c9ffb1d8e75fd6a3192f025a74240c0
Wojciech Pietrzak <wojciech.pietrzak@microsoft.wsei.edu.pl>
2026-02-27 14:49:15 +0100

    prosta funkcjonalnosc zapisywania i usuwania zrobiona

--- src/pages/ManageMe.tsx
+++ src/pages/ManageMe.tsx
@@ -1,14 +1,52 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { projectService } from '../services/projectService'
+
 function ManageMe() {
-    return (
+    const [projects, setProjects] = useState(projectService.getAll())
+    const [nazwa, setNazwa] = useState('')
+    const [opis, setOpis] = useState('')
+
+    useEffect(() => {
+        setProjects(projectService.getAll())
+    }, [])
+
+    const projectAdd = () => {
+        if (nazwa.trim() === '') {
+            alert('Nazwa nie może być pusta!')
+            return
+        }
+        if (opis.trim() === '') {
+            alert('Opis nie może być pusty!')
+            return
+        }
+        projectService.create(nazwa, opis)
+        setProjects(projectService.getAll())
+        setNazwa('')
+        setOpis('')
+    }
+
+    const projectDelete = (id: string) => {
+        projectService.delete(id)
+        setProjects(projectService.getAll())
+    }
+
+  return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-5xl font-bold text-gray-800">Manage Me</h1>
       <div className="mt-8">
-        <button className="cursor-pointer ml-4 bg-blue-500 text-white px-4 py-2 rounded">
-            Add Project
-        </button>
-        <button className="cursor-pointer ml-4 bg-red-500 text-white px-4 py-2 rounded">
-            Delete Project
-        </button>
+        <input value={nazwa} onChange={e => setNazwa(e.target.value)} placeholder="Nazwa" className="px-4 py-2 border rounded mr-2" />
+        <input value={opis} onChange={e => setOpis(e.target.value)} placeholder="Opis" className="px-4 py-2 border rounded mr-2" />
+        <button onClick={projectAdd} className="cursor-pointer px-4 py-2 bg-blue-500 text-white rounded">Dodaj projekt</button>
+      </div>
+      <div className="mt-8">
+        {projects.map((project) => (
+          <div key={project.id} className='bg-white p-4 rounded mb-2'>
+            <h3 className="font-bold mb-3">{project.nazwa}</h3>
+            <p className="mb-3">{project.opis}</p>
+            <button onClick={() => projectDelete(project.id)} className="cursor-pointer px-4 py-2 bg-red-500 text-white rounded">Usuń</button>
+          </div>
+        ))}
       </div>
     </div>
     )

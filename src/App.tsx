d790ecaa2509bd3798cf8e8d10b31c8d20f2da10
Wojciech Pietrzak <wojciech.pietrzak@microsoft.wsei.edu.pl>
2026-02-27 14:01:19 +0100

    Czysty szkielet pustego projektu

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-5xl font-bold text-gray-800">Hello World</h1>
+    </div>
+  )
+}
+
+export default App
